5b35f57ad7ca305cc139e6efe0932a4dbee8ba9c
WKcervantes <winstoncer@gmail.com>
2024-12-13 10:32:08 -0500

    archivo index

--- java.js
+++ java.js
@@ -8,3 +8,18 @@ function agregarAlCarrito(producto) {
 function mostrarCarrito() {
   console.log("Carrito de compras:", carrito);
 }
+
+function validarFormulario() {
+  let nombre = document.getElementById('nombre').value;
+  let correo = document.getElementById('correo').value;
+  let mensaje = document.getElementById('mensaje').value;
+
+  if (!nombre || !correo || !mensaje) {
+    alert("Todos los campos son obligatorios.");
+    return false;
+  }
+
+  alert("Formulario enviado con éxito");
+  // Aquí podrías agregar una acción para enviar los datos del formulario.
+  return false; // Para evitar el comportamiento por defecto de enviar el formulario
+}
